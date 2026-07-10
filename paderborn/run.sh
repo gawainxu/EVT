13aaccf53dbcc26ed92acdf57736242db39c19da
gawainxu <xgawain@gmail.com>
2026-07-10 23:57:40 +0200

    training paderborn

--- paderborn/run.sh
+++ paderborn/run.sh
@@ -1,7 +1,7 @@
-#python train_test_split.py --train_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_1" --test_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_1_test"
-#python train_test_split.py --train_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_2" --test_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_2_test"
-#python train_test_split.py --train_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_3" --test_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_3_test"
-#python train_test_split.py --train_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_4" --test_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_4_test"
+python train_test_split.py --train_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_1" --test_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_1_test"
+python train_test_split.py --train_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_2" --test_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_2_test"
+python train_test_split.py --train_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_3" --test_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_3_test"
+python train_test_split.py --train_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_4" --test_dir "/home/users/j/jiawen/datasets/paderborn/class13_0_12_4_test"
 
 
 python Train.py --data_folder "/home/users/j/jiawen/datasets/paderborn/class13_0_12_1" --model_path "/home/users/j/jiawen/EVT/paderborn/class13_0_12_1.pth"
