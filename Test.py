b876facdba7fc60a424c14b759b853516cfae252
gawainxu <xgawain@gmail.com>
2026-07-11 23:09:54 +0200

    training paderborn

--- Test.py
+++ Test.py
@@ -93,6 +93,7 @@ def PrecisionRecall(preds, labels, num_classes):
             precisions.append(0)
             continue
         class_c_idx = [i for i, x in enumerate(labels) if x == c]
+        print(len(class_c_idx))
         tp = len(np.where(preds[class_c_idx] == c))
         fp = len(np.where(preds == c)) - tp
         fn = len(class_c_idx) - tp
