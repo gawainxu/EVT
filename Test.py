9c94d15718c61030141a654df0796eac4490a539
gawainxu <xgawain@gmail.com>
2026-07-12 13:59:02 +0200

    training paderborn

--- Test.py
+++ Test.py
@@ -84,8 +84,8 @@ def PrecisionRecall(preds, labels, num_classes):
     classes.append(100)
 
     preds = np.array(preds)
-    print(len(preds))
-    print(len(labels))
+    #print(len(preds))
+    #print(len(labels))
     
     for c in classes:
         if c == 100:
